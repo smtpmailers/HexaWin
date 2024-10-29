9f0cd33993c09e343d78a8e6feb86a530a34063f
smtpmailers <sandeepsingh@smtpmailers.com>
2024-10-29 22:40:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function startCaptcha() {
 
         // Redirect to the main site after a delay
         setTimeout(() => {
-            window.location.href = "https://smtpmailers.gotrackier.com/click?campaign_id=404&pub_id=44"; // Redirect URL
+            window.location.href = "https://flipkart.com"; // Redirect URL
         }, 1500); // 1.5 seconds delay before redirect
     }, 2000); // Simulated processing time (2 seconds)
 }
